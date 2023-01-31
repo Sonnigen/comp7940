96bf9d69069f1004c68cb1e160eb6dffec9d48ca
Sonnigen <22437452@life.hkbu.edu.hk>
2023-01-31 21:01:39 +0800

    donald commit

--- lab1.py
+++ lab1.py
@@ -1,4 +1,10 @@
-def main():
-    print("Hello World")
-if __name__ == '__main__':
-    main()
+# def main():
+#     print("Hello World")
+# if __name__ == '__main__':
+#     main()
+
+# Find the all factors of x using a loop and the operator %
+# % means find remainder, for example 10 % 2 = 0; 10% 3 = 1
+x = 52633
+for i in range(x+1):
+    
